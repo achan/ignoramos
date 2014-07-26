90041f1aaee2a3eb0e43e9c2139711f4ba23044f
Amos Chan <amosschan@gmail.com>
2014-07-25 22:48:16 -0400

    `Ignoramos` dispatches options to the appropriate commands
    Currently, only the `new` command is supported and the rest will be
    dispatched to `NilCommand` which simply exits.

--- lib/ignoramos.rb
+++ lib/ignoramos.rb
@@ -1,5 +1,22 @@
+require 'commands/new_command'
+
 class Ignoramos
-  def self.hi
-    'hello world'
+  def initialize(args = [])
+    command(args).execute
+  end
+
+  def command(args)
+    cmd = args[0] unless args.empty?
+
+    if cmd == 'new'
+      NewCommand.new(args[1])
+    else
+      NilCommand.new
+    end
+  end
+
+  NilCommand = Struct.new(:args) do
+    def execute
+    end
   end
 end
