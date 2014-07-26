2b6702286bf9334a66ef594f163b0f1cfdf75c58
Amos Chan <amosschan@gmail.com>
2014-07-25 21:58:35 -0400

    adds `execute` method to generate default app structure

--- lib/commands/new_command.rb
+++ lib/commands/new_command.rb
@@ -1,7 +1,19 @@
+require 'fileutils'
+
 class NewCommand
   attr_accessor :dir
 
   def initialize(dir)
     @dir = dir.to_s
+  end
+
+  def execute
+    FileUtils.mkdir_p([
+      "#{ @dir }/_drafts",
+      "#{ @dir }/_includes",
+      "#{ @dir }/_layouts",
+      "#{ @dir }/_posts",
+      "#{ @dir }/_site"
+    ])
   end
 end
