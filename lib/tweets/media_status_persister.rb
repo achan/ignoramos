3007ff93c145fb01641a057697aa7bf55b569a21
Amos Chan <amosschan@gmail.com>
2016-10-17 10:20:07 -0400

    Make MediaStatusPersister handle no images

--- lib/tweets/media_status_persister.rb
+++ lib/tweets/media_status_persister.rb
@@ -17,7 +17,7 @@ image: {{tweet.image}}
 LAYOUT
 
   def initialize(image_path)
-    @image_path = image_path
+    @image_paths = [image_path]
     @file_helper = FileHelper.new(Dir.pwd)
   end
 
@@ -29,7 +29,7 @@ LAYOUT
                              'id' => tweet.id,
                              'url' => tweet.uri.to_s,
                              'timestamp' => DateTime.parse(tweet.created_at.to_s),
-                             'image' => copy_image(tweet.id)
+                             'image' => copy_images_to_blog(tweet.id).first
                            }
                          }))
   end
@@ -39,18 +39,19 @@ LAYOUT
   end
 
   private
-  def image
-    @image ||= File.new(@image_path)
-  end
-
-  def copy_image(tweet_id)
+  def copy_images_to_blog(tweet_id)
     FileUtils.mkdir_p("#{Dir.pwd}/img/tweets")
-    FileUtils.cp(@image_path, "#{Dir.pwd}#{conventional_filename(tweet_id)}")
-
-    conventional_filename(tweet_id)
+    @image_paths.
+      each { |p| copy_image(tweet_id, p) }.
+      map { |p| conventional_filename(tweet_id, p) }
   end
 
-  def conventional_filename(tweet_id)
-    "/img/tweets/#{tweet_id}-#{File.basename(image)}"
+  def copy_image(tweet_id, image_path)
+    FileUtils.
+      cp(image_path, "#{Dir.pwd}#{conventional_filename(tweet_id, image_path)}")
+  end
+
+  def conventional_filename(tweet_id, image_path)
+    "/img/tweets/#{tweet_id}-#{File.basename(File.new(image_path))}"
   end
 end
